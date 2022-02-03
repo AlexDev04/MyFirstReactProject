183ee8391085c705bae140cafe742a8efa8b06f8
AlexDev04 <Web.Yakovlev04@yandex.ru>
2022-02-03 18:21:53 +0300

    Project on webpack

---
+++ helloworld/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import HelloWorld from "./HelloWorld";
+
+import "./index.css";
+import "./HelloWorld.css"
+
+ReactDOM.render(
+    <HelloWorld/>,
+    document.getElementById("root")
+);
