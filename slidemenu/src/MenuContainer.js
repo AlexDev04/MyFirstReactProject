afaaae502f1e73e236179ecf4f32e8040973a56e
AlexDev04 <Web.Yakovlev04@yandex.ru>
2022-02-08 16:06:36 +0300

    Flyoutmenu

---
+++ slidemenu/src/MenuContainer.js
@@ -0,0 +1,53 @@
+import React, {Component} from "react";
+import MenuButton from "./MenuButton";
+import Menu from "./Menu";
+
+class MenuContainer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            visible: false
+        };
+
+        this.handleMouseDown = this.handleMouseDown.bind(this);
+        this.toggleMenu = this.toggleMenu.bind(this);
+    }
+
+    handleMouseDown(e) {
+        this.toggleMenu();
+
+        console.log('clicked');
+        e.stopPropagation();
+    }
+
+    toggleMenu() {
+        this.setState({
+            visible: !this.state.visible
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                <MenuButton handleMouseDown={this.handleMouseDown}/>
+                <Menu handleMouseDown={this.handleMouseDown}
+                      menuVisibility={this.state.visible}/>
+                <div>
+                    <p>It seems to be a header</p>
+                    <ul>
+                        <li>One</li>
+                        <li>Two</li>
+                        <li>Three</li>
+                        <li>Four</li>
+                        <li>Five</li>
+                        <li>Six</li>
+                        <li>Seven</li>
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default MenuContainer;
